10d1c81a3690e7c4b6982ed3bfabcb050dac4322
ElGaharbiAyoub <elgharbiay@gmail.com>
2023-06-21 14:44:28 +0100

    add json server and handle crud for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-import Button from 'react-bootstrap/Button';
-import ButtonComponent from './components/ButtonComponent';
+import { useState } from "react"
+import { v4 as uuidv4 } from 'uuid';
+import api from "./api/users"
+
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = async (user)=> {
+    const request = {
+      id: uuidv4(),
+      ...user
+    }
+    const response = await api.post("/users", request)
+    setUsers([...users, response.data])
+  }
+
+  const deleteUserHandler = async (id)=> {
+    await api.delete(`/users/${id}`);
+    const newUsersList = users.filter((user)=>{
+      return user.id !== id
+    });
+    setUsers(newUsersList);
+  }
 
   return (
     <div>
