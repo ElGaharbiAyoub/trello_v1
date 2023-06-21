42451fbaf2f36152e678b92f0b16d46cb7bfba89
ElGaharbiAyoub <elgharbiay@gmail.com>
2023-06-21 16:05:13 +0100

    add login and signup files

---
+++ src/pages/Login.jsx
@@ -0,0 +1,40 @@
+import '../styles/forms.css'
+
+function LoginForm() {
+  return (
+    <div className='wrapper bg-dark d-flex align-items-center justify-content-center w-100'>
+    <div className='login'>
+
+        <h2 className='mb-3'>login form</h2>
+        <form className='needs-validation'>
+
+        <div className='form-group was-validated mb-2'>
+            <label htmlFor='email' className='form-label'> Email address</label>
+            <input type="email" className='form-control' required></input>
+            <div className="invalid-feedback">
+                Please Enter your Email
+            </div>
+
+        </div>
+        <div className='form-group mb-2'>
+            <label htmlFor='password' className='form-label'> Password</label>
+            <input type="password" className='form-control' required ></input>
+            <div className="invalid-feedback">
+                Please Enter your password
+            </div>
+
+        </div>
+        <div className='form-group form-check mb-2'>
+        <input type="checkbox" className='form-check-input'></input>
+        <label htmlFor='check' className='form-check-label'> Remember me</label>
+
+        </div>
+
+        <button type='submit' className='btn btn-outline-info w-100 block mt-2'>LOG IN</button>
+        </form>
+    </div>
+    </div>
+  );
+}
+
+export default LoginForm;
