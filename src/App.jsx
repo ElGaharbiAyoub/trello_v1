6df431fad6dc421f1a35c9f48e5f10e55184e311
sala2022 <saloua.kasmibakkali@gmail.com>
2023-06-22 17:00:09 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,18 @@ function App() {
 
   return (
     <div style={{height: "100vh"}}>
+     {users.map((user) => (
+        <div key={user.id}>
+          <span>{user.name}</span>
+          <Button variant="danger" onClick={() => deleteUserHandler(user.id)}>
+            Delete
+          </Button>
+        </div>
+      ))}
+
+
+
+
     <Router>
       <NavComponent/>
       <Routes>
