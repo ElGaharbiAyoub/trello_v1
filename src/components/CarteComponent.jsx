b31d2b2817859b5b5d0e7d3b057cd9c6ad1e881f
sala2022 <saloua.kasmibakkali@gmail.com>
2023-06-26 11:26:45 +0200

    update task

--- src/components/CarteComponent.jsx
+++ src/components/CarteComponent.jsx
@@ -1,60 +1,99 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
+import api from '../api/users';
 import Card from 'react-bootstrap/Card';
-import '../styles/carte.css';
 import Button from 'react-bootstrap/Button';
-import api from "../api/users"
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashCan } from '@fortawesome/free-regular-svg-icons';
-import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { faEye } from '@fortawesome/free-regular-svg-icons';
-import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faTrashAlt, faEdit, faEye, faUser } from '@fortawesome/free-regular-svg-icons';
+import '../styles/carte.css';
 
-function CarteComponent({ task, users, onDelete }) {
+function CarteComponent({ task, users, onDelete, onUpdate }) {
   const filterUser = users.filter((user) => {
     return user.id === task.responsable;
   });
 
   const userName = filterUser.length > 0 ? filterUser[0].name : '';
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState(task.title);
+  const [updatedDescription, setUpdatedDescription] = useState(task.description);
+
+  const navigate = useNavigate();
+
   const handleDelete = async () => {
     try {
-      await api.delete(`/tasks/${task.id}`).then(
-        alert("deleted"))
+      await api.delete(`/tasks/${task.id}`);
+      onDelete();
+    } catch (error) {
+      console.error('Error deleting task:', error);
     }
-    
-    catch(error ){
-      alert("not deleted!!!!!")
+  };
 
-    }
-     
+  const handleUpdate = async () => {
+    setIsEditing(!isEditing);
   
-    
+    if (!isEditing) {
+      try {
+        await api.put(`/tasks/${task.id}`, {
+          title: updatedTitle,
+          description: updatedDescription
+        });
+        onUpdate(updatedTitle, updatedDescription);
+      } catch (error) {
+        console.error('Error updating task:', error);
+      }
+    }
   };
 
   return (
-    <Card style={{ width: '18rem' }} className='carte text-center m-5'>
-      <div className='enteteCarte'></div>
+    <Card style={{ width: '18rem' }} className="carte text-center m-5">
+      <div className="enteteCarte"></div>
       {userName && (
-        <span className='text-start userName fw-bold p-1'>
+        <span className="text-start userName fw-bold p-1">
           <FontAwesomeIcon icon={faUser} /> {userName}
         </span>
       )}
       <Card.Body>
-        <Card.Title className='title'>{task.title}</Card.Title>
-        <Card.Text className='description'>{task.discription}</Card.Text>
-        <Button href='#' variant='outline-success' className='btn-sm mx-1'>
+        {isEditing ? (
+          <>
+            <input
+              type="text"
+              className="form-control mb-2"
+              value={updatedTitle}
+              onChange={(e) => setUpdatedTitle(e.target.value)}
+            />
+            <textarea
+              className="form-control mb-2"
+              rows="3"
+              value={updatedDescription}
+              onChange={(e) => setUpdatedDescription(e.target.value)}
+            ></textarea>
+          </>
+        ) : (
+          <>
+            <Card.Title className="title">{task.title}</Card.Title>
+            <Card.Text className="description">{task.description}</Card.Text>
+          </>
+        )}
+        <Button href="#" variant="outline-success" className="btn-sm mx-1">
           <FontAwesomeIcon icon={faEye} />
         </Button>
-        <Button href='#' variant='outline-primary' className='btn-sm mx-1'>
-          <FontAwesomeIcon icon={faPenToSquare} />
+        <Button
+          href="#"
+          variant="outline-primary"
+          className="btn-sm mx-1"
+          onClick={handleUpdate}
+        >
+          <FontAwesomeIcon icon={faEdit} />
         </Button>
         <Button
-          href='#'
-          variant='outline-danger'
-          className='btn-sm mx-1'
-          onClick={handleDelete} 
+          href="#"
+          variant="outline-danger"
+          className="btn-sm mx-1"
+          onClick={handleDelete}
         >
-          <FontAwesomeIcon icon={faTrashCan} />
+          <FontAwesomeIcon icon={faTrashAlt} />
         </Button>
       </Card.Body>
     </Card>
